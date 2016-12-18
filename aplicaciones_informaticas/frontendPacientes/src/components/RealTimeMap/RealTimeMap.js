ad967f0bc08344910a09b7ba2c1094984c3c97e4
Ariel Wainer <ari@ariwainer.com.ar>
2016-12-18 00:13:19 -0300

    Open map in new window.

--- aplicaciones_informaticas/frontendPacientes/src/components/RealTimeMap/RealTimeMap.js
+++ aplicaciones_informaticas/frontendPacientes/src/components/RealTimeMap/RealTimeMap.js
@@ -6,7 +6,8 @@ class RealTimeMap extends Component {
   render () {
     return (
         <div className="embed-container">
-        <iframe src="http://localhost:8000/static/map.html" height="500"/>
+        <p><a href="http://localhost:8000/static/map.html">Abrir en nueva ventana</a></p>
+        <iframe src="http://localhost:8000/static/map.html" height="400"/>
         </div>
     );
   }
