52decde1fabc8af0fa24364096506c9ec78e8982
Ariel Wainer <ari@ariwainer.com.ar>
2016-12-18 23:26:58 -0300

    Charts.

--- aplicaciones_informaticas/frontendCentrosSalud/src/components/Statistics/Statistics.js
+++ aplicaciones_informaticas/frontendCentrosSalud/src/components/Statistics/Statistics.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
-import { hospitalService } from '../../services';
+import { hospitalService, staticsService } from '../../services';
 import Dropdown from 'react-toolbox/lib/dropdown';
+import { Button } from 'react-toolbox/lib/button';
 
 class Statistics extends Component {
 
-  constructor (props) {
-    super(props);
-    this.state = { healthCenters: [] };
-    this.handleHealthCenterChange = this.handleHealthCenterChange.bind(this);
-  }
+    constructor (props) {
+        super(props);
+        this.state = { healthCenters: [] };
+        this.handleHealthCenterChange = this.handleHealthCenterChange.bind(this);
+    }
 
-  componentDidMount () {
-    hospitalService.getHospitals()
-      .then(result => this.setState({ healthCenters: result.results }));
-  }
+    componentDidMount () {
+        hospitalService.getHospitals().then(result => this.setState({ healthCenters: result.results }));
 
-  handleHealthCenterChange (value) {
-    this.setState({ selectedHealthCenterId: value });
-  }
+        var url = "http://localhost:8000/foo";
+        var el = document.createElement('script');
+        el.src = url.replace('<id>', this.props.embedId);
+        this.getDOMNode().appendChild(el);
 
-  render () {
+    }
 
-    return (
-      <div>
-        <Dropdown
-          auto
-          allowBlank={true}
-          label="Seleccione hospital"
-          source={this.state.healthCenters.map(item => ({ value: item.id, label: item.name }))}
-          value={this.state.selectedHealthCenterId}
-          onChange={this.handleHealthCenterChange}
-        />
+    handleHealthCenterChange (value) {
+        this.setState({ selectedHealthCenterId: value });
+    }
+
+
+    render () {
+        return (
+            <div>
+                Estadísticas por Centro de Salud
+               <Dropdown
+                auto
+                allowBlank={true}
+                label="Seleccione hospital"
+                source={this.state.healthCenters.map(item => ({ value: item.id, label: item.name }))}
+                value={this.state.selectedHealthCenterId}
+                onChange={this.handleHealthCenterChange}
+            />
+
       </div>
     );
   }
