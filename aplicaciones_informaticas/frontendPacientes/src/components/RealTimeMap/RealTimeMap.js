84985f7ab5a0e900886e54bda2b5c3db56102ee1
AriW <awainer@gmail.com>
2016-12-18 22:58:50 -0300

    Se abre en una nueva pestana el mapa

--- aplicaciones_informaticas/frontendPacientes/src/components/RealTimeMap/RealTimeMap.js
+++ aplicaciones_informaticas/frontendPacientes/src/components/RealTimeMap/RealTimeMap.js
@@ -6,7 +6,7 @@ class RealTimeMap extends Component {
   render () {
     return (
         <div className="embed-container">
-        <p><a href="http://localhost:8000/static/map.html">Abrir en nueva ventana</a></p>
+        <p><a href="http://localhost:8000/static/map.html" target="_blank" >Abrir en nueva ventana</a></p>
         <iframe src="http://localhost:8000/static/map.html" height="400"/>
         </div>
     );
