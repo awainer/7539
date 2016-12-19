bf34242801d64e1d90098078058463fab93f3151
nanovazquez <marianodvazquez@gmail.com>
2016-12-19 11:09:29 -0300

    Fix server URL to point to the outside

--- aplicaciones_informaticas/frontendCentrosSalud/src/services/index.js
+++ aplicaciones_informaticas/frontendCentrosSalud/src/services/index.js
@@ -1,11 +1,9 @@
 import geolocationService from './geolocationService';
 import hospitalService from './hospitalService';
 import feedService from './feedService';
-import staticsService from './staticsService';
 
 export {
   geolocationService,
   hospitalService,
   feedService,
-  staticsService,
 };
